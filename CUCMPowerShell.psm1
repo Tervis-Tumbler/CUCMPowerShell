a5c56863127b7bf01eb39cb4af61682eda1509a9
fhoseinih <hosseini@tervis.com>
2016-08-30 15:48:44 -0400

    Added functions Find-CUCMUser, Find-CUCMPhone, Find-CUCMLine
    And removed some unecessary semicolons

--- CUCMPowerShell.psm1
+++ CUCMPowerShell.psm1
@@ -36,6 +36,78 @@ function Get-CUCMUser {
     $XmlContent.Envelope.Body.getUserResponse.return.user
 }
 
+    param(
+        [Parameter(Mandatory)][String]$firstName
+    )    
+        $AXL=@"
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/9.1">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ns:listUser>
+        <searchCriteria>
+              <firstName>$firstName</firstName>
+        </searchCriteria>
+        <returnedTags>
+        <userid/>
+        <telephoneNumber/>
+        </returnedTags>        
+    </ns:listUser>
+    </soapenv:Body>
+</soapenv:Envelope>
+"@
+    $XmlContent = Invoke-CUCMSOAPAPIFunction -AXL $AXL -MethodName listUser
+    $XmlContent.Envelope.Body.listUserResponse.return.user
+}
+
+    param(
+        [Parameter(Mandatory)][String]$Name
+    )    
+    $AXL=@"
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/9.1">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ns:listPhone>
+        <searchCriteria>
+              <name>$Name</name>
+        </searchCriteria>
+        <returnedTags>
+        <name/>
+        <ownerUserName/>
+        </returnedTags>        
+    </ns:listPhone>
+    </soapenv:Body>
+</soapenv:Envelope>
+"@
+    $XmlContent = Invoke-CUCMSOAPAPIFunction -AXL $AXL -MethodName listPhone
+    
+    $XmlContent.Envelope.Body.listPhoneResponse.return.phone    
+}
+
+    param(
+        [Parameter(Mandatory)][String]$Pattern
+    )    
+        $AXL=@"
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/9.1">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ns:listLine>
+        <searchCriteria>
+              <pattern>$Pattern</pattern>
+              <description>%</description> 
+        </searchCriteria>
+        <returnedTags>
+        <pattern/>
+        <description/>
+        </returnedTags>        
+    </ns:listLine>
+    </soapenv:Body>
+</soapenv:Envelope>
+"@
+    $XmlContent = Invoke-CUCMSOAPAPIFunction -AXL $AXL -MethodName listLine
+    $XmlContent
+}
+
+
 function Get-CUCMPhone {
     param(
         [Parameter(Mandatory)][String]$Name
@@ -108,7 +180,6 @@ function Invoke-CUCMSOAPAPIFunction {
         [parameter(Mandatory)]$AXL,
         [parameter(Mandatory)]$MethodName
     )
-    
     add-type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
@@ -128,16 +199,16 @@ function Invoke-CUCMSOAPAPIFunction {
     #unless you run it through fiddler to try and see what is wrong and then it works fine :(, 
     #$Result = Invoke-WebRequest -ContentType "text/xml;charset=UTF-8" -Headers @{SOAPAction="CUCM:DB ver=9.1 $MethodName";Accept="Accept: text/*"} -Body $AXL -Uri https://ter-cucm-pub1:8443/axl/ -Method Post -Credential $Credential -SessionVariable AXLWebSession
     #$XmlContent = [xml]$Result.Content
-
+    
     $WebRequest = [System.Net.WebRequest]::Create("https://ter-cucm-pub1:8443/axl/") 
     $WebRequest.Method = "POST"
     $WebRequest.ProtocolVersion = [System.Net.HttpVersion]::Version10
     $WebRequest.Headers.Add("SOAPAction","CUCM:DB ver=9.1 $MethodName")
     $WebRequest.ContentType = "text/xml"
     $WebRequest.Credentials = $Credential.GetNetworkCredential()
-    $Stream = $WebRequest.GetRequestStream();
+    $Stream = $WebRequest.GetRequestStream()
     $Body = [byte[]][char[]]$AXL
-    $Stream.Write($Body, 0, $Body.Length);
+    $Stream.Write($Body, 0, $Body.Length)
     $WebResponse = $WebRequest.GetResponse()
     $WebResponseStream = $WebResponse.GetResponseStream()
     $StreamReader = new-object System.IO.StreamReader $WebResponseStream
